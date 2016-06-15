51756bf7b5ed32d22fed89378987fd06be900900
peibibing <qingyiliu@outlook.com>
2016-06-16 07:03:49 +0800

    mult_threading

--- demo.py
+++ demo.py
@@ -1,3 +1,66 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-print(2)
+
+
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#!/date/
+import urllib
+from threading import Thread,Lock
+import requests
+from Queue import Queue
+import time
+import re
+
+class Fetcher:
+    def __init__(self,threads):
+
+        self.opener = urllib.build_opener(urllib.HTTPHandler)
+        self.lock = Lock() #线程锁
+        # self.q_req = Queue() #任务队列
+        # self.q_ans = Queue() #完成队列
+        self.threads = threads
+        for i in range(threads):
+            t = Thread(target=self.threadget)
+            t.setDaemon(True)
+            t.start()
+        self.running = 0
+
+    def __del__(self): #解构时需等待两个队列完成
+        time.sleep(0.5)
+        self.q_req.join()
+        self.q_ans.join()
+
+    def taskleft(self):
+        return self.q_req.qsize()+self.q_ans.qsize()+self.running
+
+    def push(self,req):
+        self.q_req.put(req)
+
+    def pop(self):
+        return self.q_ans.get()
+
+    def threadget(self):
+        while True:
+            req = self.q_req.get()
+            with self.lock: #要保证该操作的原子性，进入critical area
+                self.running += 1
+            try:
+                ans = self.opener.open(req).read()
+            except Exception:
+                ans = ''
+                print (ans)
+            self.q_ans.put((req,ans))
+            with self.lock:
+                self.running -= 1
+            self.q_req.task_done()
+            time.sleep(0.1) # don't spam
+
+if __name__ == "__main__":
+
+    # print links[0:10]
+    f = Fetcher(threads=100)
+
+
+
